e4620319f3234e022f97dffa15c9f26034f1049d
Amir <rezaamir731@gmail.com>
2023-11-04 20:37:48 +0330

    feat: -added a simple interface for testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,59 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import { usePost } from "../generated/swr/hooks/queries/testQuery.gql.hooks";
-import {
-  useCreateComment,
-  useCreateUserMutation,
-} from "../generated/swr/hooks/mutations/testQuery.gql.hooks";
+
+import {useGetAllTodos} from "@/generated/swr/hooks/queries/testQuery.gql.hooks";
+import {useCreateTodo} from "@/generated/swr/hooks/mutations/testQuery.gql.hooks";
+import React from "react";
+
 export default function Home() {
-  const { data, isLoading, error, mutate } = usePost({ id: "2" });
+    const {data, isLoading: isLoadingTodos} = useGetAllTodos({});
 
-  const { data: mutationData, isMutating, trigger } = useCreateComment();
 
-  const { trigger: mutation, reset } = useCreateUserMutation();
-  if (isLoading) {
-    <h1>loading...</h1>;
-  }
-  return (
-    <main style={{ padding: 40 }}>
-      <h1>Query fetched Successfully !</h1>
-      <button
-        type="button"
-        onClick={() => {
-          mutate();
-        }}
-      >
-        Refetch Query
-      </button>
-      <hr />
-      <h1>call mutation</h1>
-      <button
-        type="button"
-        onClick={() => {
-          mutation(
-            {
-              input: {
-                body: "harchi",
-                email: "test@test.com",
-                name: "myTestName",
-              },
-            },
-            {
-              onError: (err) => {
-                console.log("erroe", err);
-              },
-              onSuccess: (data) => {console.log("data", data)}
+    const {trigger, isMutating} = useCreateTodo();
+    const handleCreateTodo = (e: React.FormEvent) => {
+        e.preventDefault();
+        const inputVal = document.getElementById("todoTitle") as HTMLInputElement;
+        if (!inputVal) return;
+
+        const {value} = inputVal;
+        if (!value) return alert("please add a title for your todo!!!");
+
+        trigger({
+            input: {
+                completed: false,
+                title: value
             }
-          );
-        }}
-      >
-        mutate
-      </button>
-    </main>
-  );
+        }).then((d) => {
+
+            alert(`Todo created successfully!!! , your todo id is ${d?.createTodo?.id}`);
+        }).catch((reason) => {
+            alert("An error accord while creating a todo ☹️ ,check your console... ");
+            console.error(reason);
+        });
+
+    };
+
+
+    return (
+        <main style={{padding: 40}}>
+
+            <h1>
+                press <kbd>F12</kbd> to open your console!
+            </h1>
+
+            {(!isLoadingTodos && data?.todos?.data) && <h3>Query fetched Successfully !</h3>}
+            {isLoadingTodos && "Loading Todos ..."}
+
+            {isMutating && "Mutating Todos ..."}
+            <ul>
+                {!isLoadingTodos && data?.todos?.data?.map(d => <li key={d?.id}>{d?.title}</li>)}
+            </ul>
+
+            <form onSubmit={handleCreateTodo}>
+                <input type="text" placeholder={"todo title"} id={"todoTitle"}/>
+                <button type={"submit"}>
+                    create todo
+                </button>
+            </form>
+        </main>
+    );
 }
