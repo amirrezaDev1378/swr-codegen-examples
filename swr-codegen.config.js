e10a2d977d3ed0b35fdda1c13e131569b3b9ffe4
Amir <rezaamir731@gmail.com>
2023-11-04 20:37:09 +0330

    feat: -updated to the latest version

--- swr-codegen.config.js
+++ swr-codegen.config.js
@@ -1,26 +1,8 @@
 const fs = require("fs");
 
 module.exports = {
-    gqlGlob: "./**/*.gql",
+    gqlGlob: "[!node_module]**/*.gql",
     targetPath: "src/generated/swr/",
-    schema: "./schema.graphql",
-    gatewayAddress: "http://localhost:4000/graphql",
+    schema: "graphql/schema.graphql",
+    gatewayAddress: "https://graphqlzero.almansi.me/api",
 };
-
-// -----------
-
-// module.exports = {
-//     gqlGlob: "/**/*.gql", ADD . in aval
-//     targetPath: "./src/generated/swr",  ADD /
-//     schema: "./schema.graphql"
-// };
-
-
-// --------------
-// const fs = require("fs");
-
-// module.exports = {
-//     gqlGlob: "./**/*.gql",
-//     targetPath: "src/generated/swr/",
-//     schema: "./schema.graphql"
-// };
